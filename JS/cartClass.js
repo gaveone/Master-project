3e4111c89f16bd42c742ba91634d6c88d8e9aa4b
“gave” <adeun2020@gmail.com>
2024-10-26 23:43:15 -0700

    Setting up the card counter

--- JS/cartClass.js
+++ JS/cartClass.js
@@ -1,12 +1,21 @@
 class CartlocalStorage {
+    cartData;
     constructor() {
         this.InitializeStorage();
+        this.cartData = this.LoadData();
     }
     InitializeStorage() {
         if (!localStorage.getItem("cart")) {
             localStorage.setItem("cart", JSON.stringify([]));
             console.log("successfully created cart");
         }
+    }
+    LoadData() {
+        let data = JSON.parse(localStorage.getItem("cart") || "[]");
+        return data;
+    }
+    getSize() {
+        return this.cartData.length;
     }
 }
 export default CartlocalStorage;
--- JS/test.js
+++ JS/test.js
@@ -1,3 +1,7 @@
 import CartlocalStorage from "./cartClass.js";
 console.log("test");
 var cart = new CartlocalStorage();
+const cartCount = document.querySelector("[data-Cart-count]");
+if (cartCount) {
+    cartCount.textContent = cart.getSize().toString();
+}
