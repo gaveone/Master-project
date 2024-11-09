3766f71ec63c66c3a671b3ee0f5fdea89001e025
“gave” <adeun2020@gmail.com>
2024-11-09 11:51:32 -0800

    90% product view completion

---
+++ JS/product.js
@@ -0,0 +1,29 @@
+import IndexedDBX from "./Utility/database.js";
+const database = new IndexedDBX();
+const urlParams = new URLSearchParams(window.location.search);
+const Video = document.querySelector("[data-video]");
+const Name = document.querySelector("[data-name]");
+const Description = document.querySelector("[data-description]");
+console.log("test in the product GS");
+async function loadData() {
+    const id = urlParams.get("id");
+    console.log(id);
+    if (id) {
+        try {
+            const item = await database.GetUnique(id);
+            if (Video && Name && Description && item) {
+                // Video.setAttribute("src", item.Video || "");
+                Video.src = item.Video;
+                Name.textContent = item.name || "No Name Available";
+                Description.textContent = item.Description || "No Description Available";
+            }
+            else {
+                console.log("something went wrong");
+            }
+        }
+        catch (error) {
+            console.error("Failed to retrieve item from database:", error);
+        }
+    }
+}
+loadData();
